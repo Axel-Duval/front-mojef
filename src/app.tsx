7152d21aaf0f5c989c6b3de105db203a7a828456
BrokenSwing <fhugouvieux@gmail.com>
2021-03-20 20:59:12 +0100

    Remove useless overprotection of pages
    App pages are already protected for access by the app route

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import ProtectedRoute from "./components/ProtectedRoute";
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
 import Booking from "./pages/Booking";
@@ -12,6 +11,7 @@ import Zones from "./pages/Zones";
 import Categories from "./pages/Categories";
 import Account from "./pages/Account";
 import Accounts from "./pages/Accounts";
+import { Route } from "react-router";
 
 function App() {
   return (
@@ -20,21 +20,17 @@ function App() {
         <Navbar />
       </aside>
       <main className="uk-flex-1 uk-background-muted uk-padding-large">
-        <ProtectedRoute exact path="/app" component={Dashboard} />
-        <ProtectedRoute exact path="/app/reservations" component={Bookings} />
-        <ProtectedRoute
-          exact
-          path="/app/reservations/:id"
-          component={Booking}
-        />
-        <ProtectedRoute exact path="/app/comptabilite" component={Accounting} />
-        <ProtectedRoute exact path="/app/zones" component={Zones} />
-        <ProtectedRoute exact path="/app/societes/:id" component={Company} />
-        <ProtectedRoute exact path="/app/societes" component={Companies} />
-        <ProtectedRoute exact path="/app/jeux" component={Games} />
-        <ProtectedRoute exact path="/app/categories" component={Categories} />
-        <ProtectedRoute exact path="/app/compte" component={Account} />
-        <ProtectedRoute exact path="/app/comptes" component={Accounts} />
+        <Route exact path="/app" component={Dashboard} />
+        <Route exact path="/app/reservations" component={Bookings} />
+        <Route exact path="/app/reservations/:id" component={Booking} />
+        <Route exact path="/app/comptabilite" component={Accounting} />
+        <Route exact path="/app/zones" component={Zones} />
+        <Route exact path="/app/societes/:id" component={Company} />
+        <Route exact path="/app/societes" component={Companies} />
+        <Route exact path="/app/jeux" component={Games} />
+        <Route exact path="/app/categories" component={Categories} />
+        <Route exact path="/app/compte" component={Account} />
+        <Route exact path="/app/comptes" component={Accounts} />
       </main>
     </div>
   );
