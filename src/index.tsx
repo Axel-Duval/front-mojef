469bff33cfac9456e022c73b5c74b84fb027f718
BrokenSwing <fhugouvieux@gmail.com>
2021-03-25 10:10:09 +0100

    Add loading spinner for login

--- src/index.tsx
+++ src/index.tsx
@@ -30,110 +30,148 @@ import {
 } from "./contexts/user";
 import axios, { AxiosInstance } from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Loading from "./components/Loading";
 
-function prepareLoginState(): UserCredentials {
+function credentialsFromLocalStorage(): UserCredentials {
   const access_token = localStorage.getItem("access_token");
   const refresh_token = localStorage.getItem("refresh_token");
   return access_token && refresh_token ? { access_token, refresh_token } : null;
 }
 
-function prepareAxiosInstance(creds: UserCredentials): AxiosInstance {
+function prepareAxiosInstance(): AxiosInstance {
   return axios.create({
     baseURL: process.env.REACT_APP_API_URL || "http://localhost:3001",
     timeout: 1000,
     headers: {
       "Access-Control-Allow-Origin": "*",
       crossorigin: true,
-      Authorization: creds ? `Bearer ${creds.access_token}` : undefined,
     },
   });
 }
 
+async function refresh(
+  refresh_token: string,
+  axios: AxiosInstance
+): Promise<UserCredentials> {
+  try {
+    const res = await axios.post("/auth/refresh", {
+      refresh_token,
+    });
+    if (((res.status / 1000) | 0) !== 2) {
+      return null;
+    }
+    return res.data;
+  } catch {
+    return null;
+  }
+}
+
+function getPayloadOf(jwt: string) {
+  return JSON.parse(atob(jwt.split(".")[1]));
+}
+
+function isExpired(payload: any) {
+  return payload.exp * 1000 <= new Date().getTime();
+}
+
 function Global() {
-  const [userContext, setUserContext] = useState<UserContextValue>(() => {
-    let userCredentials = prepareLoginState();
-    const axiosInstance = prepareAxiosInstance(userCredentials);
-    if (userCredentials) {
-      const expirationDateRefresh = JSON.parse(
-        atob(userCredentials.refresh_token.split(".")[0])
-      ).exp;
-      const expRefresh = new Date(expirationDateRefresh);
-      if (expRefresh > new Date()) {
-        userCredentials = null;
+  const [loaded, setLoaded] = useState(false);
+  const [userContext, setUserContext] = useState<UserContextValue>({
+    loggedIn: false,
+    login: async (username, password) => {
+      const { data } = await userContext.axios.post("auth/login", {
+        username,
+        password,
+      });
+      userContext.setCredentials(data);
+    },
+    logout: () => {
+      userContext.setCredentials(null);
+    },
+    setCredentials: (credentials: UserCredentials) => {
+      if (credentials) {
+        localStorage.setItem("access_token", credentials.access_token);
+        localStorage.setItem("refresh_token", credentials.refresh_token);
+        setUserContext((ctx) => {
+          ctx.axios.defaults.headers[
+            "Authorization"
+          ] = `Bearer ${credentials.access_token}`;
+          return {
+            ...ctx,
+            _credentials: credentials,
+            loggedIn: true,
+            jwtPayload: getPayloadOf(credentials.access_token),
+          };
+        });
       } else {
-        axiosInstance
-          .post("/auth/refresh", {
-            refresh_token: userCredentials.refresh_token,
-          })
-          .then((res) => {
-            if (res.status === 200) {
-              userContext.setCredentials(res.data);
+        localStorage.removeItem("access_token");
+        localStorage.removeItem("refresh_token");
+        setUserContext((ctx) => {
+          delete ctx.axios.defaults.headers["Authorization"];
+          return {
+            ...ctx,
+            _credentials: credentials,
+            loggedIn: false,
+            jwtPayload: null,
+          };
+        });
+      }
+    },
+    _credentials: null,
+    axios: prepareAxiosInstance(),
+    jwtPayload: null,
+  });
+
+  // On app load
+  useEffect(() => {
+    if (!loaded) {
+      const credentials = credentialsFromLocalStorage();
+      if (!credentials) {
+        setLoaded(true);
+        return;
+      }
+      const accessTokenPayload = getPayloadOf(credentials.access_token);
+      const refreshTokenPayload = getPayloadOf(credentials.refresh_token);
+      if (isExpired(refreshTokenPayload)) {
+        setLoaded(true);
+        return;
+      }
+
+      if (isExpired(accessTokenPayload)) {
+        refresh(credentials.refresh_token, userContext.axios).then(
+          (credentials) => {
+            if (credentials) {
+              userContext.setCredentials(credentials);
             }
-          })
-          .catch(() => {
-            userContext.logout();
-          });
+            setLoaded(true);
+          }
+        );
+      } else {
+        userContext.setCredentials(credentials);
+        setLoaded(true);
       }
     }
-    return {
-      loggedIn: !!userCredentials,
-      login: async (username, password) => {
-        const { data } = await userContext.axios.post("auth/login", {
-          username,
-          password,
-        });
-        userContext.setCredentials(data);
-      },
-      logout: () => {
-        userContext.setCredentials(null);
-      },
-      setCredentials: (credentials: UserCredentials) => {
-        if (credentials) {
-          localStorage.setItem("access_token", credentials.access_token);
-          localStorage.setItem("refresh_token", credentials.refresh_token);
-          setUserContext((ctx) => {
-            ctx.axios.defaults.headers[
-              "Authorization"
-            ] = `Bearer ${credentials.access_token}`;
-            return {
-              ...ctx,
-              _credentials: credentials,
-              loggedIn: true,
-              jwtPayload: JSON.parse(
-                atob(credentials.access_token.split(".")[1])
-              ),
-            };
-          });
-        } else {
-          localStorage.removeItem("access_token");
-          localStorage.removeItem("refresh_token");
-          setUserContext((ctx) => {
-            delete ctx.axios.defaults.headers["Authorization"];
-            return {
-              ...ctx,
-              _credentials: credentials,
-              loggedIn: false,
-              jwtPayload: null,
-            };
-          });
-        }
-      },
-      _credentials: userCredentials,
-      axios: axiosInstance,
-      jwtPayload: userCredentials
-        ? JSON.parse(atob(userCredentials.access_token.split(".")[1]))
-        : null,
-    };
-  });
+  }, []);
 
   useEffect(() => {
     if (userContext._credentials) {
-      const accessTokenPayload = JSON.parse(
-        atob(userContext._credentials.access_token.split(".")[1])
+      const accessTokenPayload = getPayloadOf(
+        userContext._credentials.access_token
       );
+      const refreshTokenPayload = getPayloadOf(
+        userContext._credentials.refresh_token
+      );
+
+      // Refresh token expired, we logout
+      if (isExpired(refreshTokenPayload)) {
+        userContext.logout();
+        return;
+      }
+
       const expTime = accessTokenPayload.exp;
       const time = expTime * 1000 - new Date().getTime() - 5000;
 
+      // Planifying access_token refresh
       const id = setTimeout(() => {
         userContext.axios
           .post("/auth/refresh", {
@@ -156,15 +194,21 @@ function Global() {
 
   return (
     <UserContext.Provider value={userContext}>
-      <Router>
-        <Switch>
-          <Route path="/" exact>
-            {userContext.loggedIn ? <Redirect to="/app" /> : <Login />}
-          </Route>
-          {userContext.loggedIn ? <Route path="/app" component={App} /> : null}
-          <Redirect from="*" to="/" />
-        </Switch>
-      </Router>
+      {!loaded ? (
+        <Loading />
+      ) : (
+        <Router>
+          <Switch>
+            <Route path="/" exact>
+              {userContext.loggedIn ? <Redirect to="/app" /> : <Login />}
+            </Route>
+            {userContext.loggedIn ? (
+              <Route path="/app" component={App} />
+            ) : null}
+            <Redirect from="*" to="/" />
+          </Switch>
+        </Router>
+      )}
     </UserContext.Provider>
   );
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -48,61 +48,63 @@ function Login() {
   };
   return (
     <UserContext.Consumer>
-      {(ctx) => (
-        <form
-          id="login-layout"
-          onSubmit={(e) => {
-            e.preventDefault();
-            setLoggingIn(true);
-            ctx
-              .login(form.username.get(), form.password.get())
-              .catch(showBadCredentialsNotification)
-              .finally(() => setLoggingIn(false));
-          }}
-        >
-          <div className="uk-width-5-6 uk-width-2-3@s uk-width-1-3@m uk-width-1-4@l uk-padding">
-            <h1 className="uk-text-center uk-text-bold uk-padding-small@m">
-              Connexion
-            </h1>
-            <FormControl label="Identifiant" icon="user">
-              <input
-                className="uk-input"
-                id="input-username"
-                type="text"
-                value={form.username.get()}
-                onChange={(e) => {
-                  form.username.set(e.target.value);
-                }}
+      {(ctx) =>
+        !ctx.loggedIn && (
+          <form
+            id="login-layout"
+            onSubmit={(e) => {
+              e.preventDefault();
+              setLoggingIn(true);
+              ctx
+                .login(form.username.get(), form.password.get())
+                .catch(showBadCredentialsNotification)
+                .finally(() => setLoggingIn(false));
+            }}
+          >
+            <div className="uk-width-5-6 uk-width-2-3@s uk-width-1-3@m uk-width-1-4@l uk-padding">
+              <h1 className="uk-text-center uk-text-bold uk-padding-small@m">
+                Connexion
+              </h1>
+              <FormControl label="Identifiant" icon="user">
+                <input
+                  className="uk-input"
+                  id="input-username"
+                  type="text"
+                  value={form.username.get()}
+                  onChange={(e) => {
+                    form.username.set(e.target.value);
+                  }}
+                />
+              </FormControl>
+              <FormControl label="Mot de passe" icon="lock">
+                <input
+                  className="uk-input"
+                  id="input-password"
+                  type="password"
+                  value={form.password.get()}
+                  onChange={(e) => {
+                    form.password.set(e.target.value);
+                  }}
+                />
+              </FormControl>
+              <button
+                type="submit"
+                className="uk-button uk-button-primary uk-align-center"
+                disabled={!formErrors.$form.valid || loggingIn}
+              >
+                Se connecter
+              </button>
+            </div>
+            <div className="uk-width-1-3@l uk-visible@l">
+              <img
+                src={svg}
+                alt="Collaboration Illustration"
+                className="uk-padding-large"
               />
-            </FormControl>
-            <FormControl label="Mot de passe" icon="lock">
-              <input
-                className="uk-input"
-                id="input-password"
-                type="password"
-                value={form.password.get()}
-                onChange={(e) => {
-                  form.password.set(e.target.value);
-                }}
-              />
-            </FormControl>
-            <button
-              type="submit"
-              className="uk-button uk-button-primary uk-align-center"
-              disabled={!formErrors.$form.valid || loggingIn}
-            >
-              Se connecter
-            </button>
-          </div>
-          <div className="uk-width-1-3@l uk-visible@l">
-            <img
-              src={svg}
-              alt="Collaboration Illustration"
-              className="uk-padding-large"
-            />
-          </div>
-        </form>
-      )}
+            </div>
+          </form>
+        )
+      }
     </UserContext.Consumer>
   );
 }
